e55be8544c094d6a29c3666cb86bc5d056a992e6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-15 11:39:31 -0300

    aula

---
+++ script.js
@@ -0,0 +1,9 @@
+function calcQuadrado(){
+    let numeros = document.getElementById("numeros").value;
+    numeros = numeros.split(",");
+    for(i=0;i<numeros.length;i++){
+        quadrado = parseInt(numeros[i]) * parseInt(numeros[i]);
+        document.write("o quadrado de" +numeros[i]+ "é" +quadrado+"<br>")
+    }
+
+}
